5798e549317d205e7cf4dd91d4844b12673d0dd8
frandipradana95 <frandipradana95@gmail.com>
2025-03-15 01:24:26 +0700

    hapus test fetch api

--- src/index.js
+++ src/index.js
@@ -10,12 +10,6 @@ const store = createStore({ count: 0 });
 const Counter = () => {
 	const [count, setCount] = store.getState("count");
 
-	onMount(() => {
-		fetch("https://jsonplaceholder.typicode.com/posts")
-			.then((response) => response.json())
-			.then((json) => console.log(json));
-	});
-
 	return createElement(
 		"div",
 		null,
