2ee25b13abe962d1ba897e364402b6fdc4a5b979
frandipradana95 <frandipradana95@gmail.com>
2025-03-15 00:32:25 +0700

    membuat onMount dan test onMount di hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import createElement from "./core/createElement";
 import { render } from "./core/dom";
-import { useFlow, useGlobalState, useState } from "./core/hooks";
+import { onMount, useFlow, useGlobalState, useState } from "./core/hooks";
 import { createStore } from "./core/store";
 const store = createStore({ count: 0 });
 
 const App = () => {
 	const [count, setCount] = store.getState("count");
 
-	useFlow(() => {
+	onMount(() => {
 		fetch("https://jsonplaceholder.typicode.com/posts")
 			.then((response) => response.json())
 			.then((json) => console.log(json));
-	}, []);
+	});
 
 	return createElement(
 		"div",
